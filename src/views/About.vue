001c2f9deb891b7499477e5c6aa04a456c048efb
Jia Yu Wu <jiayuwu@Jesss-MacBook-Pro.local>
2021-12-18 10:59:26 +0800

    update home and about page 1. change nav router-link style 2. update home page image 3. update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <blockquote>
+      “Traveling – it leaves you speechless, then turns you into a storyteller.”
+      <p class="name">– Ibn Battuta</p>
+    </blockquote>
   </div>
 </template>
+
+<style>
+.about {
+  width: 100%;
+  height: calc(100% - 56px);
+}
+
+blockquote {
+  font-family: Georgia, Times, "Times New Roman", serif;
+  font-size: 3em;
+  font-style: italic;
+  line-height: 1.5em;
+  padding: 1em 2em;
+}
+
+blockquote .name {
+  font-size: 0.75em;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="hero-image">
+      <div class="hero-text">
+        <h1>Let's Travel.</h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
+
+<style>
+.home {
+  width: 100%;
+  height: calc(100% - 56px);
+}
+
+.hero-image {
+  background-image: url("../../src/assets/travel.jpeg");
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+
+.hero-text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  font-size: 3rem;
+  font-family: "Caveat", cursive;
+  text-align: center;
+  text-shadow: 3px 2px 3px rgba(255, 255, 255, 0.2);
+  color: white;
+  transform: translate(-50%, -100%);
+}
+</style>
