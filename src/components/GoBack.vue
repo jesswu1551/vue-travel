a6be0cdeecdcc6c21e2ff45aa1f80fd4b3ac139a
jess <jiayu1551@gmail.com>
2021-12-21 15:46:38 +0800

    add router goback component

---
+++ src/components/GoBack.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="goback">
+    <button @click="goBack" class="goback-btn">⬅ Go Back</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GoBack",
+  methods: {
+    goBack() {
+      return this.$router.go(-1);
+    },
+  },
+}
+</script>
+
+<style>
+.goback-btn {
+  padding: 4px 8px;
+  position: fixed;
+  top: 80px;
+  left: 24px;
+  background-color: #fff;
+  border: 1px solid #2c3e50;
+  z-index: 1;
+}
+
+.goback-btn:hover,
+.goback-btn:active {
+  color: #f2994a;
+  border: 1px solid #f2994a;
+}
+</style>
